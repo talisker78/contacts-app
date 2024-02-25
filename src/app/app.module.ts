a83e3ced6b7183b0cb3b22a1db0b0d98db555ed4
talisker78 <jimwilde@gmail.com>
2024-02-24 22:33:45 -0500

    Before FormGroups

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,7 +20,8 @@ import { InMemoryContactsApi } from './contacts/in-memory-contacts.service';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    HttpClientInMemoryWebApiModule.forRoot(InMemoryContactsApi, { delay: 200 })
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryContactsApi, { delay: 200 }),
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/edit-contact/edit-contact.component.ts
+++ src/app/edit-contact/edit-contact.component.ts
@@ -1,19 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { ContactsService } from '../contacts/contacts.service';
 
 @Component({
   templateUrl: './edit-contact.component.html',
   styleUrls: ['./edit-contact.component.css']
 })
 export class EditContactComponent implements OnInit {
-  constructor(private route: ActivatedRoute) { }
+
+  firstName = new FormControl();
+  lastName = new FormControl();
+  dateOfBirth = new FormControl();
+  favoritesRanking = new FormControl();
+
+
+  constructor(private route: ActivatedRoute, private contactsService: ContactsService) { }
 
   ngOnInit() {
     const contactId = this.route.snapshot.params['id'];
     if (!contactId) return
+
+    this.contactsService.getContact(contactId).subscribe(contact => {
+      if(!contact) return;
+      this.firstName.setValue(contact.firstName);
+      this.lastName.setValue(contact.lastName);
+      this.dateOfBirth.setValue(contact.dateOfBirth);
+      this.favoritesRanking.setValue(contact.favoritesRanking);
+    });
   }
 
   saveContact() {
+    console.log(this.firstName.value);
+    console.log(this.lastName.value);
+    console.log(this.dateOfBirth.value);
+    console.log(this.favoritesRanking.value);
 
   }
 }
