c2ae989cd55ef7aa1480de67914588750f882fa8
talisker78 <jimwilde@gmail.com>
2024-02-26 14:33:29 -0500

    End of Module 3
    Refactor edit-contact component form structure

--- src/app/edit-contact/edit-contact.component.ts
+++ src/app/edit-contact/edit-contact.component.ts
@@ -1,48 +1,58 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ContactsService } from '../contacts/contacts.service';
 
 @Component({
   templateUrl: './edit-contact.component.html',
-  styleUrls: ['./edit-contact.component.css']
+  styleUrls: ['./edit-contact.component.css'],
 })
 export class EditContactComponent implements OnInit {
-
-  contactForm = new FormGroup( {
-    firstName: new FormControl(),
-    lastName: new FormControl(),
-    dateOfBirth: new FormControl(),
-    favoritesRanking: new FormControl(),
-
+  contactForm = this.fb.nonNullable.group({
+    id: '',
+    firstName: '',
+    lastName: '',
+    dateOfBirth: <Date | null>null,
+    favoritesRanking: <number | null>null,
+    phone: this.fb.nonNullable.group({
+      phoneNumber: '',
+      phoneType: '',
+    }),
+    address: this.fb.nonNullable.group({
+      streetAddress: '',
+      city: '',
+      state: '',
+      postalCode: '',
+      addressType: '',
+    }),
   });
 
-
-
-  constructor(private route: ActivatedRoute, private contactsService: ContactsService, private router: Router ) { }
+  constructor(
+    private route: ActivatedRoute,
+    private contactsService: ContactsService,
+    private router: Router,
+    private fb: FormBuilder
+  ) {}
 
   ngOnInit() {
     const contactId = this.route.snapshot.params['id'];
-    if (!contactId) return
+    if (!contactId) return;
 
-    this.contactsService.getContact(contactId).subscribe(contact => {
-      if(!contact) return;
-      this.contactForm.controls.firstName.setValue(contact.firstName);
-      this.contactForm.controls.lastName.setValue(contact.lastName);
-      this.contactForm.controls.dateOfBirth.setValue(contact.dateOfBirth);
-      this.contactForm.controls.favoritesRanking.setValue(contact.favoritesRanking);
+    this.contactsService.getContact(contactId).subscribe((contact) => {
+      if (!contact) return;
+      this.contactForm.setValue(contact);
     });
   }
 
   saveContact() {
-    console.log("Saving contact...");
+    console.log('Saving contact...');
     this.contactsService.saveContact(this.contactForm.getRawValue()).subscribe({
       next: () => {
         console.log('Contact saved');
         this.router.navigate(['/contacts']);
       },
       error: (err: Error) => console.error('Error saving contact', err),
-      complete: () => console.log('Save contact completed')
+      complete: () => console.log('Save contact completed'),
     });
   }
 }
